8b7bf08ffedaa8605ed682c311d2f9f7c0c2ba8f
Harikant Bajaj <harikantb08@gmail.com>
2025-09-02 02:03:50 +0530

    zum

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -27,7 +27,7 @@ const Navbar = () => {
           <div className="flex">
             <div className="flex-shrink-0 flex items-center">
               <Link to="/" className="text-xl font-bold tracking-tight">
-                RideX
+                ZuM
               </Link>
             </div>
             
